300e1e7bcb5468b1c76c3100d055f0634a6f50e3
rajdeepcodeshere247 <rajdeepdasyear2006@gmail.com>
2026-06-22 18:23:31 +0530

    enable interview guides page visibility globally

--- config/features.ts
+++ config/features.ts
@@ -1,5 +1,5 @@
 export const FEATURES = {
   // Set to true to make the Guides page active and visible on the website.
   // Set to false to hide the Guides page link from the navbar and block direct URL access.
-  enableGuides: false,
+  enableGuides: true,
 };
